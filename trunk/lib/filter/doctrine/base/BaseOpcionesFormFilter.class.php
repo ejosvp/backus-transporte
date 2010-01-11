78df5ab9cec9ab0a6439f54475d5e1253c691a5b
ejosvp <ejosvp@a28847a0-cb53-460f-87ed-09a6f13e0309>
2010-01-11 02:26:37 +0000

    version 0.1

---
+++ trunk/lib/filter/doctrine/base/BaseOpcionesFormFilter.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Opciones filter form base class.
+ *
+ * @package    transporte
+ * @subpackage filter
+ * @author     ejosvp
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
+ */
+abstract class BaseOpcionesFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'name'  => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'value' => new sfWidgetFormFilterInput(),
+    ));
+
+    $this->setValidators(array(
+      'name'  => new sfValidatorPass(array('required' => false)),
+      'value' => new sfValidatorPass(array('required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('opciones_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Opciones';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'    => 'Number',
+      'name'  => 'Text',
+      'value' => 'Text',
+    );
+  }
+}
