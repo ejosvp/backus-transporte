6cad68eb92c7de1f94ad5fde9ca0ed06a27deb80
ejosvp <ejosvp@a28847a0-cb53-460f-87ed-09a6f13e0309>
2010-04-25 05:06:16 +0000

    reparado bug, con generacion de salidas falsas

--- apps/frontend/modules/salida/actions/actions.class.php
+++ apps/frontend/modules/salida/actions/actions.class.php
@@ -10,67 +10,68 @@
  */
 class salidaActions extends sfActions
 {
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->getUser()->setAttribute('activo', 'salida');
-    $this->registros = Doctrine::getTable('Registro')->getPorSalir();
-  }
+	public function executeIndex(sfWebRequest $request)
+	{
+		$this->getUser()->setAttribute('activo', 'salida');
+		$this->registros = Doctrine::getTable('Registro')->getPorSalir();
+	}
 
-  public function executeNew(sfWebRequest $request)
-  {
-    $registro = $this->getRoute()->getObject();
-    $this->form = new SalidaForm();
-    $this->form->setDefault('registro_id', $registro->getId());
-    $this->form->setDefault('chofer_id', $registro->getChofer()->getId());
-    $this->form->setDefault('tracto_id', $registro->getTracto()->getId());
-    $this->form->setDefault('carreta_id', $registro->getCarreta()->getId());
-    $this->reg_id = $registro->getId();
-  }
+	public function executeNew(sfWebRequest $request)
+	{
+		$registro = $this->getRoute()->getObject();
+		$this->form = new SalidaForm();
+		$this->form->setDefault('registro_id', $registro->getId());
+		$this->form->setDefault('chofer_id', $registro->getChofer()->getId());
+		$this->form->setDefault('tracto_id', $registro->getTracto()->getId());
+		$this->form->setDefault('carreta_id', $registro->getCarreta()->getId());
+		$this->reg_id = $registro->getId();
+	}
 
-  public function executeCreate(sfWebRequest $request)
-  {
-    $registro = Doctrine::getTable('Registro')->find($request->getParameter('reg_id'));
-    $registro->setEstado(6);
-    $registro->save();
-    $this->form = new SalidaForm();
+	public function executeCreate(sfWebRequest $request)
+	{
+		$this->registro = Doctrine::getTable('Registro')->find($request->getParameter('reg_id'));
+		
+		$this->form = new SalidaForm();
 
-    $this->processForm($request, $this->form);
+		$this->processForm($request, $this->form);
 
-    $this->reg_id = $registro->getId();
-    $this->setTemplate('new');
-  }
+		$this->reg_id = $this->registro->getId();
+		$this->setTemplate('new');
+	}
 
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->form = new SalidaForm($this->getRoute()->getObject());
-  }
+	public function executeEdit(sfWebRequest $request)
+	{
+		$this->form = new SalidaForm($this->getRoute()->getObject());
+	}
 
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->form = new SalidaForm($this->getRoute()->getObject());
+	public function executeUpdate(sfWebRequest $request)
+	{
+		$this->form = new SalidaForm($this->getRoute()->getObject());
 
-    $this->processForm($request, $this->form);
+		$this->processForm($request, $this->form);
 
-    $this->setTemplate('edit');
-  }
+		$this->setTemplate('edit');
+	}
 
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
+	public function executeDelete(sfWebRequest $request)
+	{
+		$request->checkCSRFProtection();
 
-    $this->getRoute()->getObject()->delete();
+		$this->getRoute()->getObject()->delete();
 
-    $this->redirect('salida/index');
-  }
+		$this->redirect('salida/index');
+	}
 
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $salida = $form->save();
+	protected function processForm(sfWebRequest $request, sfForm $form)
+	{
+		$form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+		if ($form->isValid())
+		{
+			$this->registro->setEstado(6);
+			$this->registro->save();
+			$salida = $form->save();
 
-      $this->redirect('salida');
-    }
-  }
+			$this->redirect('salida');
+		}
+	}
 }
